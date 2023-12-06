354d90836301e3c9395d9c46138ae0061c1d6945
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-12-06 02:27:00 +0100

    image optimized

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,11 @@
-import { getDomain } from "@/lib/utils";
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const url = getDomain();
-  console.log(url);
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${url}/sitemap.xml`,
+    sitemap: `${process.env.URL_PROD! || process.env.URL_NONPROD!}/sitemap.xml`,
   };
 }
