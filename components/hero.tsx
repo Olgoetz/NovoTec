276f6da2aad092da1b2a4b5955c8abb5691de330
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-12-07 14:07:22 +0100

    scroll behaviour

--- components/hero.tsx
+++ components/hero.tsx
@@ -87,7 +87,7 @@ export const Hero = () => {
                   />
                 )}
 
-                <p className="text-lg">{c.title}</p>
+                <p>{c.title}</p>
               </div>
             </div>
           ))}
